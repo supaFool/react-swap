c9052811d78fd71ded427bb8ac7d756409cd182a
supaFool <supafool@live.com>
2021-11-28 16:27:09 -0500

    Added trx.js to later hold TrxHistory
    Signed-off-by: supaFool <supafool@live.com>

---
+++ src/W3/Trx.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright supadev.com  (c) 2021.
+ */
+
+export default function NatTrx() {
+
+    return (
+        <div>
+            <button>Get Trx history</button>
+        </div>
+    );
+}
