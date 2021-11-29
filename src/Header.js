d22f3976a1112af489d31277712acfa6672e4dcf
supaFool <supafool@live.com>
2021-11-29 05:36:17 -0500

    Added note
    Signed-off-by: supaFool <supafool@live.com>

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,11 @@
 
 import './style/header.css';
 
+//Contains most data and login
 function Header()
 {
+
+
     const data = {
         title: "Moon Swap",
         version: "0.2.0",
@@ -13,6 +16,8 @@ function Header()
         logoutTitle: "Disconnect MetaMask from",
         loggedIn: false
     };
+
+    //TODO:Add login functions to this page
 
     return (
         <div className='App-header'>
